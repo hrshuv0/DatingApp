fc754e6cf7c4e46b62ed583ce2174abc44216641
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-16 21:09:36 +0600

    add ssl certificate

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
