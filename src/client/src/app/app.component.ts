0811d9f381032bd880c0ce3c00dda6f92ae837fa
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-16 13:24:25 +0600

    SPA - http request

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log('complete')
+    })
+  }
+
+
+
 }
