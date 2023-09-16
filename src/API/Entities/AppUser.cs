030394f591ad666b8a9017c0d8958555f69bed86
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-16 21:44:19 +0600

    extend user entity

--- src/API/Entities/AppUser.cs
+++ src/API/Entities/AppUser.cs
@@ -4,4 +4,8 @@ public class AppUser
 {
     public int Id { get; set; }
     public string UserName { get; set; }
+    
+    // This is the password hash
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
