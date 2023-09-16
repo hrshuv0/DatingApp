8a82d8426002c5e75c1f71d50347ed4f365a064d
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-16 23:23:47 +0600

    add register

---
+++ src/API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+    
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+        
+        using var hmac = new HMACSHA512();
+        
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    private async Task<bool> UserExists(object username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToString().ToLower());
+    }
+    
+    
+    
+    
+}
