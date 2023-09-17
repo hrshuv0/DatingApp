9bbcbe199263951d4dd74acf02966bd639f727fb
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-17 23:53:01 +0600

    add login

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -36,10 +36,29 @@ public class AccountController : BaseApiController
         
         return user;
     }
-
-    private async Task<bool> UserExists(object username)
+    
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
     {
-        return await _context.Users.AnyAsync(x => x.UserName == username.ToString().ToLower());
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        
+        if (user == null) return Unauthorized("Invalid username");
+        
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+        
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
     
     
