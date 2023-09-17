fe736005c8172254735355e90db41de84ee3d757
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-17 21:23:50 +0600

    add validation

--- src/API/DTOs/RegisterDto.cs
+++ src/API/DTOs/RegisterDto.cs
@@ -1,7 +1,12 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 public class RegisterDto
 {
+    [Required]
     public string Username { get; set; }
+    
+    [Required]
     public string Password { get; set; }
 }
