62ea0a4ba7e27bf026cd6a2e6c79acc19c9ad07b
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-18 22:29:38 +0600

    SPA - add register

--- src/client/src/app/_services/account.service.ts
+++ src/client/src/app/_services/account.service.ts
@@ -26,6 +26,17 @@ export class AccountService {
         );
     }
 
+    register(model: any):Observable<any> {
+        return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+            map((user: User) => {
+                if (user) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    this.currentUserSource.next(user);
+                }
+            })
+        );
+    }
+
     setCurrentUser(user: User) {
         this.currentUserSource.next(user);
     }
