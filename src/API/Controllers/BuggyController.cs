5207771d9d195b95a7f571817d0af3604d7167a9
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-19 21:43:34 +0600

    add buggy controller

---
+++ src/API/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public BuggyController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetSecret()
+    {
+        return "secret test";
+    }
+
+    [Authorize]
+    [HttpGet("not-found")]
+    public ActionResult<string> GetNotFound()
+    {
+        var thing = _context.Users.Find(-1);
+
+        if (thing == null) return NotFound();
+
+        return Ok(thing);
+    }
+
+    [Authorize]
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+        var thing = _context.Users.Find(-1);
+
+        var thingToReturn = thing!.ToString();
+
+        return Ok(thingToReturn);
+    }
+
+    [Authorize]
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+    
+}
