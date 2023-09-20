a6cf65bc288608c54cc079697b5dc5e8ecb6ff24
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-20 21:14:00 +0600

    SPA - styled member details page (left)

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -17,13 +17,11 @@ const routes: Routes = [
         canActivate: [AuthGuard],
         children: [
             {path: 'members', component: MemberListComponent},
-            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'members/:username', component: MemberDetailComponent},
             {path: 'lists', component: ListsComponent},
             {path: 'messages', component: MessagesComponent},
         ]
     },
-    {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    {path: 'members/:id', component: MemberDetailComponent},
     {path: 'errors', component: TestErrorComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: 'server-error', component: ServerErrorComponent},
--- src/client/src/app/member/member-detail/member-detail.component.ts
+++ src/client/src/app/member/member-detail/member-detail.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Member} from "../../_models/member";
+import {ActivatedRoute} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
+import {MemberService} from "../../_services/member.service";
 
 @Component({
-  selector: 'app-member-detail',
-  templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+    selector: 'app-member-detail',
+    templateUrl: './member-detail.component.html',
+    styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+    member: Member | undefined;
+
+    constructor(private memberService: MemberService,
+                private toastr: ToastrService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.loadMember();
+    }
+
+    loadMember() {
+        const username = this.route.snapshot.paramMap.get('username');
+        if (!username) return;
+
+        this.memberService.getMember(username).subscribe({
+            next: member => {
+                this.member = member;
+            }
+        })
+
+
+    }
+
 
 }
