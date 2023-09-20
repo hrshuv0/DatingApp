15351545026882596d75a941294549b1f3e4b0a3
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-20 15:24:01 +0600

    configure auto-mapper for photo-url

--- src/API/DTOs/MemberDto.cs
+++ src/API/DTOs/MemberDto.cs
@@ -4,7 +4,7 @@ public class MemberDto
 {
     public int Id { get; set; }
     public string UserName { get; set; }
-    
+    public string PhotoUrl { get; set; }
     public int Age { get; set; }
     public string KnownAs { get; set; }
     public DateTime Created { get; set; }
--- src/API/Helpers/AutomapperProfiles.cs
+++ src/API/Helpers/AutomapperProfiles.cs
@@ -8,7 +8,8 @@ public class AutomapperProfiles : Profile
 {
     public AutomapperProfiles()
     {
-        CreateMap<AppUser, MemberDto>();
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
         CreateMap<Photo, PhotoDto>();
     }
 }
