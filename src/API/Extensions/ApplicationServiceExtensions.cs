ae4c8cddb78afa45b3a8e60039615514147e045e
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-21 21:38:25 +0600

    add cloudinary

--- src/API/Extensions/ApplicationServiceExtensions.cs
+++ src/API/Extensions/ApplicationServiceExtensions.cs
@@ -19,6 +19,7 @@ public static class ApplicationServiceExtensions
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
         return services;
     }
