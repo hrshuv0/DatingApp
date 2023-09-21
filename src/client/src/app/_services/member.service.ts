e32d092c8d87c0845ecc38c16891e03aa9a5f971
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-22 01:25:10 +0600

    SPA - delete photo

--- src/client/src/app/_services/member.service.ts
+++ src/client/src/app/_services/member.service.ts
@@ -44,6 +44,10 @@ export class MemberService {
         return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
     }
 
+    deletePhoto(photoId: number) {
+        return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+    }
+
 
 
 
