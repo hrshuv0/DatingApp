4a87fb0db3e986779219f267c8b1a619ee80ec35
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-22 00:57:05 +0600

    SPA -  add photo to the nav

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -47,7 +47,7 @@ public class AccountController : BaseApiController
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+        var user = await _context.Users.Include(appUser => appUser.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
         
         if (user == null) return Unauthorized("Invalid username");
         
@@ -63,7 +63,8 @@ public class AccountController : BaseApiController
         return new UserDto
         {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)!.Url
         };
     }
 
