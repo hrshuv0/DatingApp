7d2bcfd47eb46549eb7f7ab7c1d9721a8922514b
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-23 00:55:02 +0600

    add filter by age

--- src/API/Repositories/UserRepository.cs
+++ src/API/Repositories/UserRepository.cs
@@ -60,6 +60,11 @@ public class UserRepository : IUserRepository
         var query = _context.Users.AsQueryable();
         query = query.Where(u => u.UserName != userParams.CurrentUsername);
         query = query.Where(u => u.Gender == userParams.Gender);
+        
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+        
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
         var result = query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider);
         
