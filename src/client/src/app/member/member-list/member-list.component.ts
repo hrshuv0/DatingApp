3cbb7b769655bf493795ee6e4a41b38372738011
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-22 20:09:53 +0600

    SPA - load paginated data

--- src/client/src/app/member/member-list/member-list.component.ts
+++ src/client/src/app/member/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MemberService } from "../../_services/member.service";
 import {Member} from "../../_models/member";
 import {Observable} from "rxjs";
+import {Pagination} from "../../_models/pagination";
 
 @Component({
   selector: 'app-member-list',
@@ -9,13 +10,28 @@ import {Observable} from "rxjs";
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-    members$!: Observable<Member[]>;
+    // members$!: Observable<Member[]>;
+    members: Member[] = [];
+    pagination!: Pagination;
+    pageNumber = 1;
+    pageSize = 5;
+
 
     constructor(private memberService: MemberService){
 
     }
     ngOnInit(): void {
-        this.members$ = this.memberService.getMembers();
+        // this.members$ = this.memberService.getMembers();
+        this.loadMembers();
+    }
+
+    loadMembers(){
+        this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+            next: response => {
+                this.members = response.result;
+                this.pagination = response.pagination;
+            }
+        })
     }
 
 
