b88ef8c77a085b5751090755f61569231542c738
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-23 16:03:51 +0600

    SPA - add order by

--- src/client/src/app/_modules/shared.module.ts
+++ src/client/src/app/_modules/shared.module.ts
@@ -8,6 +8,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import { FileUploadModule } from "ng2-file-upload";
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 import { PaginationModule } from "ngx-bootstrap/pagination";
+import { ButtonsModule } from "ngx-bootstrap/buttons";
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { PaginationModule } from "ngx-bootstrap/pagination";
         }),
         FileUploadModule,
         BsDatepickerModule.forRoot(),
-        PaginationModule.forRoot()
+        PaginationModule.forRoot(),
+        ButtonsModule.forRoot()
     ],
     exports:[
         BsDropdownModule,
@@ -36,7 +38,8 @@ import { PaginationModule } from "ngx-bootstrap/pagination";
         NgxSpinnerModule,
         FileUploadModule,
         BsDatepickerModule,
-        PaginationModule
+        PaginationModule,
+        ButtonsModule
     ]
 })
 export class SharedModule {
--- src/client/src/app/_services/member.service.ts
+++ src/client/src/app/_services/member.service.ts
@@ -21,6 +21,7 @@ export class MemberService {
         params = params.append('minAge', userParams.minAge);
         params = params.append('maxAge', userParams.maxAge);
         params = params.append('gender', userParams.gender);
+        params = params.append('orderBy', userParams.orderBy);
 
         return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
     }
