af0b973a4a41da4b9b562598a7eb173e19d329d7
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-23 15:51:18 +0600

    add order by

--- src/API/Helpers/UserParams.cs
+++ src/API/Helpers/UserParams.cs
@@ -14,5 +14,6 @@ public class UserParams
     public string Gender { get; set; }
     public int MinAge { get; set; } = 18;
     public int MaxAge { get; set; } = 100;
+    public string OrderBy { get; set; } = "lastActive";
     
 }
--- src/API/Repositories/UserRepository.cs
+++ src/API/Repositories/UserRepository.cs
@@ -65,6 +65,12 @@ public class UserRepository : IUserRepository
         var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
         
         query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+        
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
 
         var result = query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider);
         
