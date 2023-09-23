480fc515f767527c7c00b1c16abfd1cc25c96cb4
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-23 18:42:28 +0600

    SPA - cache member list

--- src/client/src/app/_services/member.service.ts
+++ src/client/src/app/_services/member.service.ts
@@ -12,18 +12,29 @@ import {UserParams} from "../_models/userParams";
 export class MemberService {
     baseUrl = environment.apiUrl;
     members: Member[] = [];
+    memberCache = new Map();
 
     constructor(private http: HttpClient) {
     }
 
     getMembers(userParams: UserParams): Observable<any> {
+        const response = this.memberCache.get(Object.values(userParams).join('-'));
+        if(response){
+            return of(response);
+        }
+
         let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
         params = params.append('minAge', userParams.minAge);
         params = params.append('maxAge', userParams.maxAge);
         params = params.append('gender', userParams.gender);
         params = params.append('orderBy', userParams.orderBy);
 
-        return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params);
+        return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params).pipe(
+            map(response => {
+                this.memberCache.set(Object.values(userParams).join('-'), response);
+                return response;
+            })
+        );
     }
 
     private getPaginatedResult<T>(url: string, params: HttpParams) {
