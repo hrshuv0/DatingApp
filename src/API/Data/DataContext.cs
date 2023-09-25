24914c042f2b4b79ce9dac59e39018e9ac12ee3c
hrshuv0 <h.r.shuvo9@gmail.com>
2023-09-25 20:22:59 +0600

    add message entity

--- src/API/Data/DataContext.cs
+++ src/API/Data/DataContext.cs
@@ -27,9 +27,21 @@ public class DataContext : DbContext
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+            .HasOne(u => u.Recipient)
+            .WithMany(m => m.MessagesReceived)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(m => m.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+        
     }
 
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 }
--- src/API/Entities/AppUser.cs
+++ src/API/Entities/AppUser.cs
@@ -23,5 +23,8 @@ public class AppUser
 
     public List<UserLike> LikedByUsers { get; set; }
     public List<UserLike> LikedUsers { get; set; }
+    
+    public List<Message> MessagesSent { get; set; }
+    public List<Message> MessagesReceived { get; set; }
 
 }
