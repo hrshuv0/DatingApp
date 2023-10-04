5e774aac1cdc64a4491d40d1491a3bd277758de9
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-04 21:56:18 +0600

    SPA - load user roles

---
+++ src/client/src/app/_services/admin.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment.development";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../_models/user";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminService {
+    baseUrl = environment.apiUrl;
+
+
+    constructor(private http: HttpClient) {
+    }
+
+    getUsersWithRoles() {
+        return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+    }
+
+
+}
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -29,6 +29,8 @@ import { DatePickerComponent } from './_forms/date-picker/date-picker.component'
 import { MemberMessagesComponent } from './member/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 
 @NgModule({
     declarations: [
@@ -50,7 +52,9 @@ import { HasRoleDirective } from './_directives/has-role.directive';
         DatePickerComponent,
         MemberMessagesComponent,
         AdminPanelComponent,
-        HasRoleDirective
+        HasRoleDirective,
+        UserManagementComponent,
+        PhotoManagementComponent
     ],
     imports: [
         BrowserModule,
