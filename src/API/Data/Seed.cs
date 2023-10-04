f2b535fb1c99c3031e6ad92ea96bd5fcb51ad0eb
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-04 18:48:20 +0600

    seed roles

--- src/API/Data/Seed.cs
+++ src/API/Data/Seed.cs
@@ -9,12 +9,24 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
         var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+        var roles = new List<AppRole>()
+        {
+            new AppRole() { Name = "Member" },
+            new AppRole() { Name = "Admin" },
+            new AppRole() { Name = "Moderator"},
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
 
         if (users == null) return;
 
@@ -23,6 +35,17 @@ public class Seed
             user.UserName = user.UserName!.ToLower();
 
             var success = await userManager.CreateAsync(user, "1234");
+            if (!success.Succeeded) continue;
+            
+            await userManager.AddToRoleAsync(user, "Member");
         }
+        
+        var admin = new AppUser()
+        {
+            UserName = "admin"
+        };
+        
+        await userManager.CreateAsync(admin, "1234");
+        await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
     }
 }
