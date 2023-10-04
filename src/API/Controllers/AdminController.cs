36ebbdc393653777932fc85cf5e88a7e3619f021
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-04 19:33:38 +0600

    add policy and get users with roles

---
+++ src/API/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AdminController : BaseApiController
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    
+    [Authorize(Policy = "RequireAdminRole")]
+    [HttpGet("users-with-roles")]
+    public async Task<ActionResult> GetUsersWithRoles()
+    {
+        var users = await _userManager.Users.OrderBy(u => u.UserName)
+            .Select(u => new
+            {
+                u.Id,
+                Username = u.UserName,
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+        
+        return Ok(users);
+    }
+    
+    [Authorize(Policy = "ModeratePhotoRole")]
+    [HttpGet("photos-to-moderate")]
+    public ActionResult GetPhotosForModeration()
+    {
+        return Ok("Admins or moderators can see this");
+    }
+    
+    public ActionResult ApprovePhoto(int photoId)
+    {
+        return Ok("Admins or moderators can approve photos");
+    }
+    
+    public ActionResult RejectPhoto(int photoId)
+    {
+        return Ok("Admins or moderators can reject photos");
+    }
+    
+    
+}
--- src/API/Extensions/IdentityServiceExtensions.cs
+++ src/API/Extensions/IdentityServiceExtensions.cs
@@ -38,6 +38,12 @@ public static class IdentityServiceExtensions
                     ValidateAudience = false
                 };
             });
+        
+        services.AddAuthorization(opt =>
+        {
+            opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+            opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+        });
 
         return services;
     }
