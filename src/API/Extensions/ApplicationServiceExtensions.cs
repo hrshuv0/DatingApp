341460ec89e8e1d3a9c04db208cd1bacd5430ecd
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-06 00:13:09 +0600

    real-time active status

--- src/API/Extensions/ApplicationServiceExtensions.cs
+++ src/API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Interfaces;
 using API.Repositories;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -25,6 +26,7 @@ public static class ApplicationServiceExtensions
         services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<IMessageRepository, MessageRepository>();
         services.AddSignalR();
+        services.AddSingleton<PresenceTracker>();
 
         return services;
     }
---
+++ src/API/SignalR/PresenceTracker.cs
@@ -0,0 +1,53 @@
+﻿namespace API.SignalR;
+
+public class PresenceTracker
+{
+    private static readonly Dictionary<string, List<string>> OnlineUsers = new();
+
+    public Task UserConnected(string username, string connectedId)
+    {
+        lock (OnlineUsers)
+        {
+            if (OnlineUsers.ContainsKey(username)!)
+            {
+                OnlineUsers[username].Add(connectedId);
+            }
+            else
+            {
+                OnlineUsers.Add(username, new List<string>{connectedId});
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+    
+    
+    public Task UserDisconnected(string username, string connectedId)
+    {
+        lock (OnlineUsers)
+        {
+            if (!OnlineUsers.ContainsKey(username)!) return Task.CompletedTask;
+            
+            OnlineUsers[username].Remove(connectedId);
+            if (OnlineUsers[username].Count == 0)
+            {
+                OnlineUsers.Remove(username);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+    
+    public Task<string[]> GetOnlineUsers()
+    {
+        string[] onlineUsers;
+        lock (OnlineUsers)
+        {
+            onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+        }
+
+        return Task.FromResult(onlineUsers);
+    }
+    
+    
+}
