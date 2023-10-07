f9e3c683734e6a9385b12ba8fcc667307dbc3765
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-07 20:47:03 +0600

    fix - time

--- src/API/Helpers/AutomapperProfiles.cs
+++ src/API/Helpers/AutomapperProfiles.cs
@@ -21,5 +21,8 @@ public class AutomapperProfiles : Profile
         CreateMap<Message, MessageDto>()
             .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
             .ForMember(d => d.RecipientPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
+        
+        CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
     }
 }
