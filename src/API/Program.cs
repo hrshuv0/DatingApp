b8c572cc2acf147cd7c655054dad5491ada11233
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-07 20:28:25 +0600

    add real-time read message

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -45,6 +45,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
+    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception e)
--- src/API/SignalR/MessageHub.cs
+++ src/API/SignalR/MessageHub.cs
@@ -29,6 +29,7 @@ public class MessageHub : Hub
         var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
 
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        await AddToGroup(groupName);
 
         var messages = await _messageRepository
             .GetMessageThread(Context.User.GetUsername(), otherUser);
@@ -38,9 +39,10 @@ public class MessageHub : Hub
         await base.OnConnectedAsync();
     }
 
-    public override Task OnDisconnectedAsync(Exception exception)
+    public override async Task OnDisconnectedAsync(Exception exception)
     {
-        return base.OnDisconnectedAsync(exception);
+        await RemoveFromMessageGroup();
+        await base.OnDisconnectedAsync(exception);
     }
 
     public async Task SendMessage(CreateMessageDto createMessageDto)
@@ -64,6 +66,14 @@ public class MessageHub : Hub
             Content = createMessageDto.Content
         };
         
+        var groupName = GetGroupName(sender.UserName, recipient.UserName);
+        var group = await _messageRepository.GetMessageGroup(groupName);
+
+        if (group.Connections.Any(x => x.Username == recipient.UserName))
+        {
+            message.DateRead = DateTime.UtcNow;
+        }
+        
         _messageRepository.AddMessage(message);
         var isSuccess = await _messageRepository.SaveAllAsync();
 
@@ -71,9 +81,8 @@ public class MessageHub : Hub
             throw new HubException("Failed to send message");
         
         var result = _mapper.Map<MessageDto>(message);
-        var group = GetGroupName(sender.UserName, recipient.UserName);
         
-        await Clients.Group(group).SendAsync("NewMessage", result);
+        await Clients.Group(groupName).SendAsync("NewMessage", result);
     }
 
     private string GetGroupName(string caller, string other)
@@ -81,4 +90,33 @@ public class MessageHub : Hub
         var stringCompare = string.CompareOrdinal(caller, other) < 0;
         return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
     }
+
+    private async Task<bool> AddToGroup(string groupName)
+    {
+        var group = await _messageRepository.GetMessageGroup(groupName);
+        var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
+
+        if (group is null)
+        {
+            group = new Group(groupName);
+            _messageRepository.AddGroup(group);
+        }
+        
+        group.Connections.Add(connection);
+
+        return await _messageRepository.SaveAllAsync();
+    }
+    
+    private async Task RemoveFromMessageGroup()
+    {
+        var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+        _messageRepository.RemoveConnection(connection);
+        await _messageRepository.SaveAllAsync();
+    }
+    
+    private async Task<Group> GetMessageGroup(string groupName)
+    {
+        return await _messageRepository.GetMessageGroup(groupName);
+    }
+    
 }
