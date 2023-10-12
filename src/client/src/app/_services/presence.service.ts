9f3eb43c248945ba280f1a7f1e2b579108c3cb41
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-13 03:21:02 +0600

    SPA - add message notification

--- src/client/src/app/_services/presence.service.ts
+++ src/client/src/app/_services/presence.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment.development";
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {ToastrService} from "ngx-toastr";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, take} from "rxjs";
+import {Router} from "@angular/router";
 
 @Injectable({
     providedIn: 'root'
@@ -13,7 +14,7 @@ export class PresenceService {
     private onlineUsersSource = new BehaviorSubject<string[]>([]);
     onlineUsers$ = this.onlineUsersSource.asObservable();
 
-    constructor(private toastr: ToastrService) {
+    constructor(private toastr: ToastrService, private router: Router) {
     }
 
     createHubConnection(user: any) {
@@ -36,6 +37,15 @@ export class PresenceService {
 
         this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
             this.onlineUsersSource.next(usernames);
+        });
+
+        this.hubConnection.on('NewMessageReceived', ({username, knownAs}) => {
+            this.toastr.info(knownAs + ' has sent you a new message!')
+                .onTap
+                .pipe(take(1))
+                .subscribe({
+                    next: () => this.router.navigateByUrl('/members/' + username + '?tab=Messages'),
+                });
         });
 
     }
