e5b6000a9895f1b0c229664b59e145ea9ec3b9ce
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-13 03:39:35 +0600

    fix - active status notification

--- src/API/SignalR/PresenceHub.cs
+++ src/API/SignalR/PresenceHub.cs
@@ -16,20 +16,24 @@ public class PresenceHub : Hub
     
     public override async Task OnConnectedAsync()
     {
-        await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
-        await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+        var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+        
+        if (isOnline)
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
         
         var currentUsers = await _tracker.GetOnlineUsers();
-        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+        await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
     }
 
     public override async Task OnDisconnectedAsync(Exception exception)
     {
-        await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
-        await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+        var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
         
-        var currentUsers = await _tracker.GetOnlineUsers();
-        await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+        if (isOffline)
+            await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+        
+        // var currentUsers = await _tracker.GetOnlineUsers();
+        // await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         
         await base.OnDisconnectedAsync(exception);
     }
--- src/API/SignalR/PresenceTracker.cs
+++ src/API/SignalR/PresenceTracker.cs
@@ -4,8 +4,10 @@ public class PresenceTracker
 {
     private static readonly Dictionary<string, List<string>> OnlineUsers = new();
 
-    public Task UserConnected(string username, string connectedId)
+    public Task<bool> UserConnected(string username, string connectedId)
     {
+        var isOnline = false;
+        
         lock (OnlineUsers)
         {
             if (OnlineUsers.ContainsKey(username)!)
@@ -15,26 +17,32 @@ public class PresenceTracker
             else
             {
                 OnlineUsers.Add(username, new List<string>{connectedId});
+                isOnline = true;
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOnline);
     }
     
-    public Task UserDisconnected(string username, string connectedId)
+    public Task<bool> UserDisconnected(string username, string connectedId)
     {
+        var isOffline = false;
+        
         lock (OnlineUsers)
         {
-            if (!OnlineUsers.ContainsKey(username)!) return Task.CompletedTask;
+            if (!OnlineUsers.ContainsKey(username)!) 
+                return Task.FromResult(isOffline);
             
             OnlineUsers[username].Remove(connectedId);
+            
             if (OnlineUsers[username].Count == 0)
             {
                 OnlineUsers.Remove(username);
+                isOffline = true;
             }
         }
 
-        return Task.CompletedTask;
+        return Task.FromResult(isOffline);
     }
     
     public Task<string[]> GetOnlineUsers()
