a318355b2e9cf480a64cda15730e5226ce841cf1
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-13 03:39:48 +0600

    SPA - fix active status notification

--- src/client/src/app/_services/presence.service.ts
+++ src/client/src/app/_services/presence.service.ts
@@ -28,11 +28,19 @@ export class PresenceService {
         this.hubConnection.start().catch(error => console.log(error));
 
         this.hubConnection.on('UserIsOnline', username => {
-            this.toastr.info(username + ' has connected');
+            this.onlineUsers$.pipe(take(1)).subscribe({
+                next: usernames => {
+                    this.onlineUsersSource.next([...usernames, username]);
+                }
+            });
         });
 
         this.hubConnection.on('UserIsOffline', username => {
-            this.toastr.warning(username + ' has disconnected');
+            this.onlineUsers$.pipe(take(1)).subscribe({
+                next: usernames => {
+                    this.onlineUsersSource.next(usernames.filter(x => x !== username));
+                }
+            });
         });
 
         this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
