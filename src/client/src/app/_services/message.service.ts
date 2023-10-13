3ce4cabf503bd5193c3a34d6188cb0ff959fb694
hrshuv0 <h.r.shuvo9@gmail.com>
2023-10-13 18:52:25 +0600

    SPA - optimize message

--- src/client/src/app/_services/message.service.ts
+++ src/client/src/app/_services/message.service.ts
@@ -6,6 +6,7 @@ import {Message} from "../_models/message";
 import {HubConnection, HubConnectionBuilder} from "@microsoft/signalr";
 import {User} from "../_models/user";
 import {BehaviorSubject, take} from "rxjs";
+import {Group} from "../_models/group";
 
 @Injectable({
     providedIn: 'root'
@@ -32,6 +33,22 @@ export class MessageService {
 
         this.hubConnection.on('ReceiveMessageThread', messages => {
             this.messageThreadSource.next(messages);
+        });
+
+        this.hubConnection.on('UpdatedGroup', (group: Group) => {
+            if(group.connections.some(x => x.username === otherUsername)){
+                this.messageThread$.pipe(take(1)).subscribe({
+                    next: messages => {
+                        messages.forEach(message => {
+                            if(!message.dateRead){
+                                message.dateRead = new Date(Date.now());
+                            }
+                        });
+                        this.messageThreadSource.next([...messages]);
+                    }
+                });
+            }
+
         });
 
         this.hubConnection.on('NewMessage', message => {
